1b4644e83c649e4a330ec5a09ffdbb4359ff89cf
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 12:25:32 +0100

    task 5

--- 05/Textarea.js
+++ 05/Textarea.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 class Textarea extends React.Component {
+    shouldResizeTextarea() {
+        const textarea = this.textarea;
+        const isOverflowing = textarea.offsetHeight < textarea.scrollHeight;
+        const isWithinHeightLimit = textarea.offsetHeight < 100;
+        return isOverflowing && isWithinHeightLimit;
+    }
+    resizeTextarea() {
+        const textarea = this.textarea;
+        textarea.style.height = `${textarea.scrollHeight}px`;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        return { resize: this.shouldResizeTextarea() };
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (snapshot.resize) {
+            this.resizeTextarea();
+        }
+    }
+
     render() {
-        return <textarea></textarea>
+        return (
+            <textarea
+                ref={(element) => (this.textarea = element)}
+                value={this.props.content}
+                onChange={this.props.onChange}
+            ></textarea>
+        );
     }
 }
-
-export default Textarea;
--- 05/app.js
+++ 05/app.js
@@ -6,13 +6,14 @@ import Textarea from './Textarea';
 class App extends React.Component {
     state = {
         text: '',
-    }
+    };
+    handleTextChange = (event) => {
+        this.setState({ text: event.target.value });
+    };
 
     render() {
         const { text } = this.state;
-        return (
-            <Textarea content={ text } />
-        )
+        return <Textarea content={text} onChange={this.handleTextChange} />;
     }
 }
 
